af191361b8b9eacebc9bfa9b79ece6d45ed26773
leo-ckl <leonardo.chonkan@autodesk.com>
2022-05-14 22:41:20 -0600

    simple viewer added, working ok
    Added viewer v7 and set up transparent background for the viewer and gray background for the cointainer.

--- src/components/Forge/Viewer/Viewer.js
+++ src/components/Forge/Viewer/Viewer.js
@@ -2,10 +2,12 @@ import React, { useEffect, useContext } from 'react';
 import { initializeViewer } from './viewer-helper';
 import { AuthContext } from '../../../context/auth-provider';
 
+import './Viewer.css';
+
 const Viewer = () => {
   const authCtx = useContext(AuthContext);
 
-  const urn = 'dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLnhnZ1pRdkNBUjFtVzNWWnFyN1B3NWc_dmVyc2lvbj0z';
+  const urn = 'dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLndWY0xsRE5LUWVXdFctc2pQX0RUX1E_dmVyc2lvbj0x';
 
   useEffect(() => {
     if (authCtx.credentials.access_token) {
@@ -15,8 +17,8 @@ const Viewer = () => {
 
   return (
     <div>
-      <h2>Forge Viewer</h2>
-      <div id='viewerContainer'></div>
+      {/* <h2>Forge Viewer</h2> */}
+      <div className='dsk-viewing-viewer' id='viewerContainer'></div>
     </div>
   );
 };
--- src/components/Forge/Viewer/viewer-helper.js
+++ src/components/Forge/Viewer/viewer-helper.js
@@ -15,14 +15,34 @@ export const initializeViewer = async (urn, token) => {
     accessToken: token,
     api: 'derivativeV2',
   };
+
+  //!loading extensions
+  // var config3D = {
+  //   extensions: ['Autodesk.Viewing.Wireframes'],
+  // };
+
   var viewerContainer = document.getElementById('viewerContainer');
   var viewer = new Autodesk.Viewing.Private.GuiViewer3D(viewerContainer, {});
+
+  Autodesk.Viewing.Private.InitParametersSetting.alpha = true; //enable alpha for the renderer
 
   Autodesk.Viewing.Initializer(viewerOptions, () => {
     viewer.start();
     Autodesk.Viewing.Document.load(`urn:${urn}`, (doc) => {
       var defaultModel = doc.getRoot().getDefaultGeometry();
       viewer.loadDocumentNode(doc, defaultModel);
+      // viewer.setBackgroundColor(0, 100, 0, 0, 0, 0);
+      viewer.setLightPreset(2);
+      viewer.setQualityLevel(true, true);
+      // viewer.setTheme('dark-theme');
+
+      //!Making the background transparent
+      viewer.impl.renderer().setClearAlpha(0); //clear alpha channel
+      viewer.impl.glrenderer().setClearColor(0xffffff, 0); //set transparent background, color code does not matter
+      // viewer.impl.invalidate(true); //trigger rendering
+
+      //!removing the shadow
+      viewer.setGroundShadow(false);
     });
   });
 };
